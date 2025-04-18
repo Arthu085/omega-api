6a6e95da1b1126236c0d466fa896bcf444c93c35
Joao Espindola <joaovitorfespindola@gmail.com>
2025-04-18 20:25:00 -0300

    lombok to model forno

--- src/main/java/com/omega/api/models/Forno.java
+++ src/main/java/com/omega/api/models/Forno.java
@@ -3,15 +3,13 @@ package com.omega.api.models;
 import com.omega.api.enums.SituacaoForno;
 import com.omega.api.enums.StatusForno;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Data
 @Entity
 @SequenceGenerator(name = "FORNOS_ID_SEQ", sequenceName = "FORNOS_ID_SEQ", allocationSize = 1, schema = "omega")
 @Table(name = "fornos", schema = "omega")

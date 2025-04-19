9aab7353b22198a64e89a7fefc36e3e02b031685
Joao Espindola <joaovitorfespindola@gmail.com>
2025-04-19 10:04:19 -0300

    Revert "lombok to model forno"
    This reverts commit 6a6e95da1b1126236c0d466fa896bcf444c93c35.

--- src/main/java/com/omega/api/models/Forno.java
+++ src/main/java/com/omega/api/models/Forno.java
@@ -3,13 +3,15 @@ package com.omega.api.models;
 import com.omega.api.enums.SituacaoForno;
 import com.omega.api.enums.StatusForno;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Data
 @Entity
 @SequenceGenerator(name = "FORNOS_ID_SEQ", sequenceName = "FORNOS_ID_SEQ", allocationSize = 1, schema = "omega")
 @Table(name = "fornos", schema = "omega")

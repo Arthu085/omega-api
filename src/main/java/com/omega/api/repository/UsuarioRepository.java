928e8f1eb62618d46d8486fe1fa32d07879268db
carloseduardob <carloseduardobezfontana@gmail.com>
2025-05-07 09:29:36 -0300

    deixando o get user paginavel

--- src/main/java/com/omega/api/repository/UsuarioRepository.java
+++ src/main/java/com/omega/api/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.omega.api.repository;
 
 import com.omega.api.models.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 
     Optional<Usuario> findByEmail(String email);
+
+    Page<Usuario> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 }
